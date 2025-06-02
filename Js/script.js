a2d39fff645fbe8a5dcffbcb69086e68fc566494
supungim <supunsgjgii1234@gmail.com>
2025-06-02 13:51:15 +0530

    Description Update

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+
+
+var typed = new Typed(".text", {
+    strings: ["Fullstack Developer", "Web Developer","AI and ML Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
